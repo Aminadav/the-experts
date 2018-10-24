1ac5358a556d67c3d588eca6087f71f70a947251
Aminadav Glickshtein <AminadavG@gmail.com>
2018-10-24 03:29:53 +0300

    a

---
+++ index.js
@@ -0,0 +1,167 @@
+var names = `👔 המומחים - מנהלי אתרים
+👔 המומחים - מנהלי טכנולוגיה
+👔 המומחים - פיתוח עסקי
+👔 המומחים - למידה מלאכותית (AI, Machine Learning)
+👔 המומחים - מנהלי מוצר
+👔 המומחים - מפתחי וינדוס
+👔 המומחים - אופטימיזציית אתרים
+👔 המומחים - מדעי המחשב אלגוריתמיקה ומבני נתונים
+👔 המומחים - חיפוש עבודה
+👔 המומחים - JavaScript
+👔 המומחים - אנדראיד - מפתחים
+👔 המומחים - וינדוס
+👔 המומחים - לינוקס
+👔 המומחים - פייסבוק - API
+👔 המומחים - קידום ופרסום בפייסבוק - SEO
+👔 המומחים - שיחות חברים
+👔 המומחים - אבטחת סייבר (Cyber Security)
+👔 המומחים - בלוגרים, כתיבה ומחקר בנושאי דיגיטל
+👔 המומחים - בלוקציין (Blockchain)
+👔 המומחים - NodeJS
+👔 המומחים - iOS
+👔 המומחים - Flutter
+👔 המומחים - Go
+👔 המומחים - PHP
+👔 המומחים - Python
+👔 המומחים - C#
+👔 המומחים - SQL
+👔 המומחים - NoSQL
+👔 המומחים - DevOps
+👔 המומחים - Containers - Docker, Kubernetes, and others
+👔 המומחים - השקעות
+👔 המומחים - שותפים
+👔 המומחים - משפט וחוקים לסטארטאפים
+👔 המומחים - קוד פתוח
+👔 המומחים - עיצוב וחווית משתמש
+👔 המומחים - מימון המונים (HeadStart, Kickstarter, Indiegogo)
+👔 המומחים - גוגל תג מנגר (Google Tag Manager)
+👔 המומחים - גוגל אנליטיקס (Google Analytics)
+👔 המומחים - המלצות לאפליקציות
+👔 המומחים - רשתות (Networking, Routers, Gateways, Iptable,...)
+👔 המומחים - React
+👔 המומחים - מפתחי מובייל (Mobile Developers)
+👔 המומחים - שיווק ומכירות (Online Marketing)
+👔 המומחים - גיוס וכוח אדם
+👔 המומחים - React
+👔 המומחים - Java
+👔 המומחים - TypeScript
+👔 המומחים - Agile Development
+👔 המומחים - יש לי רעיון לסטארטאפ
+👔 המומחים - דפי נחיתה
+👔 המומחים - Google Sheets
+👔 המומחים - Google Docs
+👔 המומחים - Google Slides
+👔 המומחים - המלצות קניית חומרה וחלקי מחשב
+👔 המומחים - Photoshop
+👔 המומחים - Microsoft SQL Server
+👔 המומחים - MySQL
+👔 המומחים - Chrome Extensions
+👔 המומחים - Redis
+👔 המומחים - MongoDB
+👔 המומחים - Google Cloud
+👔 המומחים - Amazon Web Services
+👔 המומחים - Microsoft Azure
+👔 המומחים - Jenkins
+👔 המומחים - Shopify
+👔 המומחים - WordPress
+👔 המומחים - BigQuery`
+	.split('\n')
+var links =
+	`https://www.facebook.com/groups/2299031410367261/
+https://www.facebook.com/groups/369604693779064/
+https://www.facebook.com/groups/2087636041288669/
+https://www.facebook.com/groups/1877038795749321/
+https://www.facebook.com/groups/694736034246259/
+https://www.facebook.com/groups/182272715988493/
+https://www.facebook.com/groups/346976009370680/
+https://www.facebook.com/groups/1804514579671046/
+https://www.facebook.com/groups/1954300584639481/
+https://www.facebook.com/groups/281854169115503/
+https://www.facebook.com/groups/443069492885365/
+https://www.facebook.com/groups/452541335271094/
+https://www.facebook.com/groups/915581228776773/
+https://www.facebook.com/groups/1912642095697593/
+https://www.facebook.com/groups/1058791317614108/
+https://www.facebook.com/groups/2237235796288580/
+https://www.facebook.com/groups/281863202662789/
+https://www.facebook.com/groups/337877340305561/
+https://www.facebook.com/groups/1103952649772653/
+https://www.facebook.com/groups/314603666001172/
+https://www.facebook.com/groups/301557397334105/
+https://www.facebook.com/groups/303707930218724/
+https://www.facebook.com/groups/755536954794217/
+https://www.facebook.com/groups/370783263463570/
+https://www.facebook.com/groups/1990052507722012/
+https://www.facebook.com/groups/681315072252006/
+https://www.facebook.com/groups/2379673258714916/
+https://www.facebook.com/groups/860286370970052/
+https://www.facebook.com/groups/209147143332261/
+https://www.facebook.com/groups/294724814698781/
+https://www.facebook.com/groups/306657883448830/
+https://www.facebook.com/groups/720453405003369/
+https://www.facebook.com/groups/185197499060654/
+https://www.facebook.com/groups/261175494601707/
+https://www.facebook.com/groups/261175494601707/
+https://www.facebook.com/groups/494179777768186/
+https://www.facebook.com/groups/215482449382785/
+https://www.facebook.com/groups/289226741716344/
+https://www.facebook.com/groups/758367731165156/
+https://www.facebook.com/groups/311129132805805/
+https://www.facebook.com/groups/262545017735998/
+https://www.facebook.com/groups/527579217664806/
+https://www.facebook.com/groups/695907647435724/
+https://www.facebook.com/groups/254771728719955/
+https://www.facebook.com/groups/1181475508671438/
+https://www.facebook.com/groups/2173671739563657/
+https://www.facebook.com/groups/251928022171928/
+https://www.facebook.com/groups/550561332072063/
+https://www.facebook.com/groups/1127681987387770/
+https://www.facebook.com/groups/1828460090613986/
+https://www.facebook.com/groups/243492643207385/
+https://www.facebook.com/groups/245716576104643/
+https://www.facebook.com/groups/2123669974330455/
+https://www.facebook.com/groups/241353919874171/
+https://www.facebook.com/groups/737979663563744/
+https://www.facebook.com/groups/165840024357921/
+https://www.facebook.com/groups/186478658936020
+https://www.facebook.com/groups/701878553530978/
+https://www.facebook.com/groups/184374172479037/
+https://www.facebook.com/groups/312527052903749
+https://www.facebook.com/groups/552898908486879/
+https://www.facebook.com/groups/2149784218671350/
+https://www.facebook.com/groups/681374005582083/
+https://www.facebook.com/groups/1157780994398290/
+https://www.facebook.com/groups/246803202651914/
+https://www.facebook.com/groups/471094800045750/
+https://www.facebook.com/groups/990937931091194/`
+		.split('\n')
+for (i = 0; i < links.length; i++) {
+	var currentLink = links[i]
+	document.write(`
+			<a class="fb-group-manual" href="javascript:click(${i})">
+			<div class="fb-group-name" >${names[i]}</div>
+			</a>
+			`)
+}
+
+document.body.addEventListener('click', function () {
+	ifrs = document.getElementsByClassName('ifr')
+	if (ifrs.length > 0) {
+		document.body.removeChild(ifrs[0])
+		document.getElementsByClassName('overflow')[0].style.display = 'none'
+	}
+})
+function click(i) {
+	ifr = document.createElement('iframe')
+	ifr.className = "ifr"
+	ifr.setAttribute('src', `join.html?link=${links[i]}`)
+	document.body.appendChild(ifr)
+	document.getElementsByClassName('overflow')[0].style.display = 'block'
+}
+		// for (i = 0; i < links.length; i++) {
+		// 	var currentLink = links[i]
+		// 	document.write(`
+		// 	<div  class="fb-group" data-href="${currentLink}" data-width="280"
+		// 	data-show-social-context="true" data-show-metadata="false"></div>
+		// 	`)
+		// }
